7dfe3ea9bbed83fbcee7851969009ab2c82fa201
julio jijon <jijonj@berea.edu>
2026-03-22 02:03:45 -0400

    changed cors

--- backend/main.py
+++ backend/main.py
@@ -35,6 +35,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://vandyhacksproject-msql.vercel.app",
+        "https://vandyhacksproject1.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
